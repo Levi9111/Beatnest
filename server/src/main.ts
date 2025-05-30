33a71683cb4f9f078f9e352ebd792d85a071f8f6
levi9111 <captainlevi9111@gmail.com>
2025-05-30 20:38:16 +0600

    credetials updated temporarily

--- server/src/main.ts
+++ server/src/main.ts
@@ -13,7 +13,7 @@ async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
 
   app.enableCors({
-    origin: [process.env.CLIENT_URL!.replace(/\/$/, '')],
+    origin: 'https://beatnest-eight.vercel.app',
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization', 'Cookie'],
